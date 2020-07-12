a7a27ba24896d57581c7e333d3e7a873c80a77b0
Alessandro Dorigo <you@example.com>
2020-07-12 12:44:09 +0200

    added the commands o compile every new script up to tcl, and removed the swap file left behind by vim

--- automating_lfs.sh
+++ automating_lfs.sh
@@ -2,9 +2,11 @@
 
 # this script is only for compiling and setting up the archives in the sources folder
 
-sh ./Binutils/binutils_pass1.sh || exit
-cd ..
-sh ./GCC/GCC_pass1.sh || exit
-cd ..
-sh ./Linux_API/Linux_API_Headers.sh || exit
-cd ..
+bash $LFS_SCRIPT/Binutils/binutils_pass1.sh || exit
+bash $LFS_SCRIPT/GCC/GCC_pass1.sh || exit
+bash $LFS_SCRIPT/Linux_API/Linux_API_Headers.sh || exit
+bash $LFS_SCRIPT/glibc/glibc.sh || exit
+bash $LFS_SCRIPT/GCC/Libstdc++.sh || exit
+bash $LFS_SCRIPT/Binutils/binutils_pass2.sh || exit
+bash $LFS_SCRIPT/GCC/GCC_pass2.sh || exit
+bash $LFS_SCRIPT/Tcl/tcl_pass1.sh || exit
